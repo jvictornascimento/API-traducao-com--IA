0462ea0281dcf4a647cb1e3d442e8b034d902aa4
jvictornascimento <victojoao789@gmail.com>
2024-07-19 22:11:00 -0300

    Init project

---
+++ src/main/java/com/jnascimento/cafeIntelectual/CafeIntelectualApplication.java
@@ -0,0 +1,13 @@
+package com.jnascimento.cafeIntelectual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeIntelectualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeIntelectualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jnascimento/cafeIntelectual/CafeIntelectualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnascimento.cafeIntelectual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeIntelectualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
