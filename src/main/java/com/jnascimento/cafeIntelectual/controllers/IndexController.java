a5333d1ecb3d1c3cbdc352d477e9424e02c66144
jvictornascimento <victojoao789@gmail.com>
2024-07-19 23:08:50 -0300

    Add image to view

--- src/main/java/com/jnascimento/cafeIntelectual/controllers/IndexController.java
+++ src/main/java/com/jnascimento/cafeIntelectual/controllers/IndexController.java
@@ -1,15 +1,23 @@
 package com.jnascimento.cafeIntelectual.controllers;
 
+import com.jnascimento.cafeIntelectual.services.CafeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    @Autowired
+    CafeService service;
     @GetMapping
     public ModelAndView index(){
-        return new ModelAndView("index");
+        var imagemUrl = service.obterCafe();
+        var model = Map.of("imagemUrl",imagemUrl.file());
+        return new ModelAndView("index",model);
     }
 }
